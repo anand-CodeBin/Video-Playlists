af27eec29ee737b73fe5e5fc0a6639d0801eba0c
anand-CodeBin <anand@code-b.in>
2022-05-17 12:51:49 +0530

    Revised #1

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -43,7 +43,7 @@ const LoginPage = () => {
 		dispatch(rememberMe(value));
 	};
 	const signInHandle = () => {
-		if (inputPass === "123") {
+		if (inputPass === "123" && inputEmail !== "") {
 			dispatch(signIn(inputEmail));
 		} else {
 			updateWrongCredentials(true);
